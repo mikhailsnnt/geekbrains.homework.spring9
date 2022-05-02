0dd3e3b30f1f2104339d68f06d2fbe052049bdf4
Mikhail Nikitin <nitinin.miha@icloud.com>
2022-05-02 12:23:54 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/service/UserService.java
@@ -0,0 +1,17 @@
+package ru.geekbrains.service;
+
+import org.springframework.data.domain.Page;
+import ru.geekbrains.dto.UserDto;
+
+import java.util.Optional;
+
+public interface UserService {
+
+    Page<UserDto> findUsersByFilter(String usernameFilter, String emailFilter, Integer page, Integer size, String sortField);
+
+    Optional<UserDto> findById(long id);
+
+    UserDto save (UserDto user);
+
+    void deleteById(long id);
+}
